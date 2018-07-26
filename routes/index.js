b5a26197b25d3f976fcc422a3be84885d18c121a
Tommy Nguyen <tommynguyen.164@gmail.com>
2018-07-26 04:08:31 +0000

    Add active tab feature on navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 //root route
 router.get('/', function(req, res){
-    res.render("home");
+    res.render("home",{page: "home"});
+});
+
+//contact route
+router.get('/contact', function(req, res){
+    res.render("contact", {page: "contact"});
 });
 
 
