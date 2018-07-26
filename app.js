525819c58066a92b7db1f88a6709c93ed5cba2ac
Tommy Nguyen <tommynguyen.164@gmail.com>
2018-07-26 00:25:19 +0000

    fix promise handling rejection issue by including local host number in mongodb url

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var express    = require('express'),
 var indexRoutes= require("./routes/index");
 
 
-var url = process.env.DATABASEURL || "mongodb://localhost/classy_nails"
-mongoose.connect(url);
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/classy_nails"
+mongoose.connect(url, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", 'ejs');
 
