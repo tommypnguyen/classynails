3fa13c4bbc0edec9af4b9e5b986dccd9633bb813
Tommy Nguyen <tommynguyen.164@gmail.com>
2018-07-26 09:17:43 +0000

    add all routes and initial pages for routes

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,14 @@ router.get('/contact', function(req, res){
     res.render("contact", {page: "contact"});
 });
 
+//service route
+router.get('/service', function(req, res){
+    res.render("services",{page:"service"});
+});
+
+//Image route
+router.get('/work', function(req, res){
+    res.render("work", {page:"work"});
+});
 
 module.exports= router;
